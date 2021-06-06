a91864028333df800e440706911d67ee659730f0
anas-abusaif <anasabusief@gmail.com>
2021-06-06 17:33:05 +0300

    added js and html

---
+++ frontPage/javascript.js
@@ -0,0 +1,11 @@
+let userName=prompt('Welcome to the wbsite, please enter your name.')
+console.log(userName)
+let favSport=prompt('what is your favorite sport?')
+console.log(favSport)
+let favSnackAfter=prompt('what is your favorite snack to have after exercising?')
+console.log(favSnackAfter)
+let situation=prompt('do you have your snack right after exercising or do you cool off and relax first?')
+console.log(situation)
+
+alert(`it's nice to have you here ${userName}, the ${favSport} sounds like a good habbit to maintain a healthy lifestyle and the ${favSnackAfter} is great idea for a snack when you ${situation}`)
+
